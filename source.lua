f0fd03447d7e62d501190f6d1c7f4276103f6e98
Astro <81985653+a4strro@users.noreply.github.com>
2023-10-14 13:43:17 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,4 +1,20 @@
-while true do
-    Beep(0.5)
-    wait(10)
+local Screen = GetPartFromPort(1, "TouchScreen")
+local ScreenSize = Screen:GetDimensions()
+
+local Keyboard = GetPartFromPort(2, "Keyboard")
+local Speaker = GetPartFromPort(3, "Speaker")
+local Disk = GetPartFromPort(4, "Disk")
+
+function MakeDesktop()
+    local Desktop = Screen:CreateElement("TextLabel", {Size=UDim2.fromScale(1, 0), BackgroundColor=BrickColor.Black(), BorderSizePixel=0, Font="Gotham", TextColor3=Color3.fromRGB(255, 255, 255), Text="zephyrOS has booted"})
+end
+
+function Init()
+    local success, result = pcall(function()
+        MakeDesktop()
+    end)
+
+    if not success then
+        Beep(1)       
+    end
 end
